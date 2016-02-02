9b748f1d17c3eaedef00ff509286d8851187c3a1
Gabru678 <Paulbath7@gmail.com>
2016-02-02 00:11:01 -0500

    Last successful build to submit

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -8,9 +8,7 @@ namespace assignment1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            static void Main(string[] args)
+       static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our wacky and wild wine list! \n");
 
@@ -23,25 +21,36 @@ namespace assignment1
             {
                 // reading in the .CSV file
                 // Find it using ../../../datafiles
+
+                CSVProcessor csvProcessr = new CSVProcessor();
+
+
+                //Dont know why this isnt working?
+                csvProcessr.ImportCSV("../../../datafiles.csv", allTheWines);
             }
             else if (UserIn == 2)
             {
                 // add to the list
                 // use a method created in the WineItemCollection
+
+                Console.WriteLine(" We will now add a new Item to the list!");
             }
             else if (UserIn == 3)
             {
                 // loop through the array by the first index of a newly created WineItem with the input
                 // as the first index and when they == each other
                 // print out that array
+                Console.WriteLine(" We will now look for an ID in the list!");
             }
             else if (UserIn == 4)
             {
+                Console.WriteLine(" We will see you later! ");
+
                 Environment.Exit(0);
             }
 
 
-        }
+            }
         }
     }
-}
+
--- assignment1/WineItemCollection.cs
+++ assignment1/WineItemCollection.cs
@@ -18,6 +18,15 @@ namespace assignment1
         // have to add a mtehod to find one that the user wanted in here or WineItem
         public int FindWine()
         {
+            Console.WriteLine("Enter what ID you are looking for: ");
+            int findID = int.Parse(Console.ReadLine());
+            // int IDNum;
+            // int index = 0;
+
+            //return WineArray.First<IDNum>;
+
+            //placeholder
+            return findID;
 
         }
 
@@ -26,7 +35,32 @@ namespace assignment1
 
         public int AddWine()
         {
+            Console.WriteLine("Ok we are going to have to get some information first. \n Please enter the ID number.");
+            //Would also like to check if its a valid input soon
+            int IDAdd = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Now enter the Description: ");
+            string DescriptionAdd = Console.ReadLine();
+
+            Console.WriteLine("Finally enter in an amount in each wine.");
+            decimal PackAdd = decimal.Parse(Console.ReadLine());
+
+            //Create a new wine Item and add it to the array
+            foreach (WineItem ToBeAdding in WineArray)
+            {
+                if (ToBeAdding != null)
+                {
+                    //int counter;
+                    //counter++;
+
+                }
+            }
+
+            //Welp that doestn work
+            //return Console.WriteLine(" The Wine with\n ID: " +  IDAdd + "\n Description: " + DescriptionAdd + "\n Pack: " + PackAdd + "\n Has been added to the list!");
+
+            //Placeholder
+            return IDAdd;
         }
     }
 }
